ebce6398305fbf6ce0cb15bed63440f390fbda22
sigmund <simonefazi2@gmail.com>
2023-10-13 17:33:29 +0200

    Modificata accessibilita' restController

--- src/main/java/com/majorbit/controller/UserController.java
+++ src/main/java/com/majorbit/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.majorbit.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import com.majorbit.model.User_Trello_Fazi;
 import com.majorbit.repository.UserDAO;
 
 @RestController
+@CrossOrigin("*")
 public class UserController {
 
 	@Autowired 
