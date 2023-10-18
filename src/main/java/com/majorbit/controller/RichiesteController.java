119c6fdae450d12afb4de3e27dce4f634e6d6c66
sigmund <simonefazi2@gmail.com>
2023-10-18 11:36:24 +0200

    Aggiunta CrossOrigin al controller delle richieste

--- src/main/java/com/majorbit/controller/RichiesteController.java
+++ src/main/java/com/majorbit/controller/RichiesteController.java
@@ -3,6 +3,7 @@ package com.majorbit.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.majorbit.repository.RichiesteDAO;
 import com.majorbit.repository.UserDAO;
 
 @RestController
+@CrossOrigin("*")
 public class RichiesteController {
 
 	@Autowired
